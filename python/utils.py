6eda2353edfdc41bad1419ff88224444f5478a65
GregorSchroeder <gsc@sandag.org>
2025-04-29 11:15:17 -0700

    #84 - PR Feedback (ASE Python)

--- python/utils.py
+++ python/utils.py
@@ -1,3 +1,5 @@
+import iteround
+import pandas as pd
 import pathlib
 import sqlalchemy as sql
 import yaml
@@ -47,6 +49,7 @@ GIS_ENGINE = sql.create_engine(
 # Other SQL configuration
 GIS_SERVER = _secrets["sql"]["gis"]["server"]
 
+
 #########################
 # RUNTIME CONFIGURATION #
 #########################
@@ -67,3 +70,111 @@ input_parser.parse_config()
 RUN_INSTRUCTIONS = input_parser.run_instructions
 RUN_ID = input_parser.run_id
 MGRA_VERSION = input_parser.mgra_version
+
+
+#####################
+# UTILITY FUNCTIONS #
+#####################
+
+
+def _2d_integerize(
+    df: pd.DataFrame, row_crtls: list, col_crtls: list, condition: str = "exact"
+) -> pd.DataFrame:
+    """Integerize values in a DataFrame based on marginal control totals.
+
+    Default behavior is to match the marginal controls exactly. If the
+    condition is set to "less than", the function will adjust values such that
+    the row values are less than or equal to the row marginal controls. The
+    column values always match the column marginal controls.
+
+    Args:
+        df (pd.DataFrame): DataFrame containing the values to be integerized.
+        row_crtls (list): Row marginal control totals.
+        col_crtls (list): Column marginal control totals.
+        condition (str): Condition for integerization. Options are 'exact' or
+            'less than'. Default is 'exact'.
+
+    Returns:
+        pd.DataFrame: DataFrame with integerized values.
+    """
+    # Ensure the marginal controls match dimensions of the DataFrame
+    if df.shape[0] != len(row_crtls) or df.shape[1] != len(col_crtls):
+        raise ValueError("Marginal controls do not match DataFrame dimensions.")
+
+    # Condition parameter checks
+    if condition not in ["exact", "less than"]:
+        raise ValueError("Condition must be 'exact' or 'less than'.")
+    elif condition == "exact" and sum(row_crtls) != sum(col_crtls):
+        raise ValueError("Marginal controls are inconsistent for 'exact' match.")
+    elif condition == "less than" and sum(col_crtls) > sum(row_crtls):
+        raise ValueError("Marginal controls are inconsistent for 'less than' match.")
+
+    # Safe round the columns to match the marginal controls
+    for i, col in enumerate(df.columns):
+        df[col] = iteround.saferound(df[col], places=0, topline=col_crtls[i])
+
+    # Calculate deviations from the row marginal controls
+    # Intialize list to store column adjustments made
+    row_devs = df.sum(axis=1) - row_crtls
+    adjustments = [0] * df.shape[1]
+
+    # Calculate the deviation condition
+    if condition == "exact":
+        any_deviation = max(map(abs, row_devs)) > 0
+    elif condition == "less than":
+        any_deviation = max(row_devs) > 0
+    else:
+        raise ValueError("Condition must be 'exact' or 'less than'.")
+
+    # While there are deviations to adjust
+    while any_deviation or max(map(abs, adjustments)) > 0:
+        # For rows with + deviation
+        for i, row in enumerate(row_devs):
+            if row > 0:
+                # Calculate adjustment as minimum of total possible and smallest non-zero column value
+                # Adjust the column value downward and store adjustments made for that column
+                col_idx = df.iloc[i].where(df.iloc[i] > 0).idxmin(skipna=True)
+                j = df.columns.get_loc(col_idx)
+                adjustment = min(row, df.iat[i, j])
+                df.iat[i, j] -= adjustment
+                adjustments[j] += adjustment
+
+        # For rows with - deviation
+        for i, row in enumerate(row_devs):
+            # Stop if no available column adjustments
+            if max(adjustments) > 0:
+                if row < 0:
+                    # Restrict to columns with available adjustments
+                    cols = [j for j, v in enumerate(adjustments) if v > 0]
+
+                    # If all values in adjustment columns are 0 skip the row
+                    if df.iloc[i, cols].max() <= 0:
+                        continue
+                    else:
+                        # Find the column with the largest non-zero value and available adjustment
+                        # Calculate adjustment as minimum of total possible row and column adjustments
+                        # Adjust the column value upward and store adjustments made for that column
+                        col_idx = df.iloc[i, cols].idxmax()
+                        j = df.columns.get_loc(col_idx)
+                        adjustment = min(abs(row), adjustments[j])
+                        df.iat[i, j] += adjustment
+                        adjustments[j] -= adjustment
+            else:
+                break
+
+        # If no changes were made avoid infinite loop
+        if row_devs.equals(df.sum(axis=1) - row_crtls):
+            raise ValueError("No adjustments made. Check marginal controls.")
+        else:
+            # Recalulate the row deviations
+            row_devs = df.sum(axis=1) - row_crtls
+
+        # Recalculate the deviation condition
+        if condition == "exact":
+            any_deviation = max(map(abs, row_devs)) > 0
+        elif condition == "less than":
+            any_deviation = max(row_devs) > 0
+        else:
+            raise ValueError("Condition must be 'exact' or 'less than'.")
+
+    return df
