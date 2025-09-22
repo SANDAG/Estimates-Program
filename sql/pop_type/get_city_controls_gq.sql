bf4d75def59da5e634b6c267c3d5e08f728ddb7a
GregorSchroeder <gsc@sandag.org>
2025-09-22 16:46:53 -0700

    #78 - replace tabs with spaces

--- sql/pop_type/get_city_controls_gq.sql
+++ sql/pop_type/get_city_controls_gq.sql
@@ -5,43 +5,43 @@ DECLARE @year integer = :year;
 
 -- Get group quarters controls from California DOF Estimates -----------------
 with [dof] AS (
-	-- E-8 Estimates - 2010-2019
-	SELECT
-		[year],
-		CASE
+    -- E-8 Estimates - 2010-2019
+    SELECT
+    	[year],
+    	CASE
             WHEN [area_name] = 'Balance of County' THEN 'Unincorporated'
-			ELSE [area_name]
+    		ELSE [area_name]
         END AS [city],
-		[group_quarters]
-	FROM [socioec_data].[ca_dof].[estimates_e8]
-	WHERE
-		[estimates_id] = 24  -- E-8: January 2025
-		AND [fips] = '06073'  -- San Diego County
-		AND [year] != 2020  -- Use the E-5 Estimates for 2020+
-		AND [area_name] NOT IN ('Total Incorporated' , 'Incorporated', 'County Total')
+    	[group_quarters]
+    FROM [socioec_data].[ca_dof].[estimates_e8]
+    WHERE
+    	[estimates_id] = 24  -- E-8: January 2025
+    	AND [fips] = '06073'  -- San Diego County
+    	AND [year] != 2020  -- Use the E-5 Estimates for 2020+
+    	AND [area_name] NOT IN ('Total Incorporated' , 'Incorporated', 'County Total')
 
-	UNION ALL
+    UNION ALL
 
-	-- E-5 Estimates - 2020+
-	SELECT
-		[year],
-		CASE
+    -- E-5 Estimates - 2020+
+    SELECT
+    	[year],
+    	CASE
             WHEN [area_name] = 'Balance of County' THEN 'Unincorporated'
             WHEN [area_name] = 'National City' THEN 'National City'
-			ELSE REPLACE([area_name], ' City', '')
+    		ELSE REPLACE([area_name], ' City', '')
         END AS [city],
-		[group_quarters]
-	FROM [socioec_data].[ca_dof].[estimates_e5]
-	WHERE
-		[estimates_id] = 25  -- E-5: Vintage 2025 (2025.5.1)
-		AND [fips] = '06073'  -- San Diego County
-		AND [area_name] NOT IN ('Total Incorporated' , 'Incorporated', 'County Total')
+    	[group_quarters]
+    FROM [socioec_data].[ca_dof].[estimates_e5]
+    WHERE
+    	[estimates_id] = 25  -- E-5: Vintage 2025 (2025.5.1)
+    	AND [fips] = '06073'  -- San Diego County
+    	AND [area_name] NOT IN ('Total Incorporated' , 'Incorporated', 'County Total')
 )
 SELECT
-	@run_id AS [run_id],
-	@year AS [year],
-	[city],
-	'Group Quarters' AS [metric],
-	[group_quarters] AS [value]
+    @run_id AS [run_id],
+    @year AS [year],
+    [city],
+    'Group Quarters' AS [metric],
+    [group_quarters] AS [value]
 FROM [dof]
 WHERE [year] = @year
