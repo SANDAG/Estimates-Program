6efc388123ec0eeaf19831440fd45903d4886fd8
GregorSchroeder <gsc@sandag.org>
2026-07-31 10:41:35 -0700

    #281 - update QCEW sql to use SANDAG employment category function

--- sql/employment/get_region_qcew.sql
+++ sql/employment/get_region_qcew.sql
@@ -46,214 +46,54 @@ DROP TABLE IF EXISTS [#qcew_result_set];
 
 
 -- Calculate custom SANDAG employment categories using quarterly data --------
-WITH [qtr_data] AS (
-    SELECT
-        CASE
-            WHEN [industry_code] IN (
-                '61',
-                '62',
-                '71',
-                '721',
-                '722'
-            ) THEN 'Total Covered'
-            ELSE [ownership_title]
-        END AS [ownership_title],
-        CASE
-            WHEN [ownership_title] = 'Private' AND [industry_code] IN (
-                '11',
-                '21',
-                '22',
-                '23',
-                '31-33',
-                '42',
-                '44-45',
-                '48-49',
-                '51',
-                '52',
-                '53',
-                '54',
-                '55',
-                '56',
-                '81'
-            ) THEN [industry_code]
-            WHEN [industry_code] IN (
-                '61',
-                '62',
-                '71',
-                '721',
-                '722'
-            ) THEN [industry_code]
-            WHEN [ownership_title] = 'Federal Government' THEN NULL
-            WHEN [ownership_title] = 'State Government' THEN NULL
-            WHEN [ownership_title] = 'Local Government' THEN NULL
-        END AS [industry_code],
-	    [month1_emplvl] + [month2_emplvl] + [month3_emplvl] AS [jobs]
-    FROM [socioec_data].[bls].[qcew_by_area_quarterly]
-    INNER JOIN [socioec_data].[bls].[industry_code]
-	    ON [qcew_by_area_quarterly].[naics_id] = [industry_code].[naics_id]
-    INNER JOIN [socioec_data].[bls].[ownership_titles]
-	    ON [qcew_by_area_quarterly].[own_code] = [ownership_titles].[ownership_code]
-    WHERE
-        [area_fips] = '06073'
-        AND [year] = @year
-        -- Restrict to Private, Federal, State, Local ownership
-        AND [ownership_title] IN (
-            'Private',
-            'Federal Government',
-            'State Government',
-            'Local Government'
-        )
-        -- Restrict to two-digit NAICS codes
-        AND [industry_code] IN (
-            '11',
-            '21',
-            '22',
-            '23',
-            '31-33',
-            '42',
-            '44-45',
-            '48-49',
-            '51',
-            '52',
-            '53',
-            '54',
-            '55',
-            '56',
-            '61',
-            '62',
-            '71',
-            '721',
-            '722',
-            '81',
-            '92'
-        )
-)
 SELECT
-    [ownership_title],
-    [industry_code],
-    -- Averaging and rounding occurs at final reporting step
-    -- See https://github.com/SANDAG/BLS/issues/55
-    ROUND(SUM([jobs])/12.0, 0) AS [jobs]
-INTO [#qcew_result_set]
-FROM [qtr_data]
+    [fn_get_sandag_employment].[ownership_title],
+    [fn_get_sandag_employment].[industry_code],
+	ROUND(SUM([month1_emplvl] + [month2_emplvl] + [month3_emplvl])/12.0, 0) AS [jobs]
+--INTO [#qcew_result_set]
+FROM [socioec_data].[bls].[qcew_by_area_quarterly]
+INNER JOIN [socioec_data].[bls].[industry_code]
+	ON [qcew_by_area_quarterly].[naics_id] = [industry_code].[naics_id]
+INNER JOIN [socioec_data].[bls].[ownership_titles]
+	ON [qcew_by_area_quarterly].[own_code] = [ownership_titles].[ownership_code]
+CROSS APPLY [socioec_data].[bls].[fn_get_sandag_employment]([ownership_title], [industry_code])
+WHERE
+    [area_fips] = '06073'
+    AND [year] = @year
+    AND [fn_get_sandag_employment].[ownership_title] IS NOT NULL
+    AND [fn_get_sandag_employment].[industry_code] IS NOT NULL
+    -- Remove the "Private" ownership only categories
+    -- These are published directly in the annual QCEW
+    AND [fn_get_sandag_employment].[ownership_title] != 'Private'
 GROUP BY
-    [ownership_title],
-    [industry_code]
--- Remove the "Private" ownership only categories
--- These are published directly in the annual QCEW
-HAVING [ownership_title] != 'Private'
-ORDER BY
-    [ownership_title],
-    [industry_code];
+    [fn_get_sandag_employment].[ownership_title],
+    [fn_get_sandag_employment].[industry_code]
 
+    UNION ALL
 
 -- Calculate directly derived employment categories using annual data --------
-WITH [annual_data] AS (
-    SELECT
-        CASE
-            WHEN [industry_code] IN (
-                '61',
-                '62',
-                '71',
-                '721',
-                '722'
-            ) THEN 'Total Covered'
-            ELSE [ownership_title]
-        END AS [ownership_title],
-        CASE
-            WHEN [ownership_title] = 'Private' AND [industry_code] IN (
-                '11',
-                '21',
-                '22',
-                '23',
-                '31-33',
-                '42',
-                '44-45',
-                '48-49',
-                '51',
-                '52',
-                '53',
-                '54',
-                '55',
-                '56',
-                '81'
-            ) THEN [industry_code]
-            WHEN [industry_code] IN (
-                '61',
-                '62',
-                '71',
-                '721',
-                '722'
-            ) THEN [industry_code]
-            WHEN [ownership_title] = 'Federal Government' THEN NULL
-            WHEN [ownership_title] = 'State Government' THEN NULL
-            WHEN [ownership_title] = 'Local Government' THEN NULL
-        END AS [industry_code],
-	    [annual_avg_emplvl] AS [jobs]
-    FROM [socioec_data].[bls].[qcew_by_area_annual]
-    INNER JOIN [socioec_data].[bls].[industry_code]
-	    ON [qcew_by_area_annual].[naics_id] = [industry_code].[naics_id]
-    INNER JOIN [socioec_data].[bls].[ownership_titles]
-	    ON [qcew_by_area_annual].[own_code] = [ownership_titles].[ownership_code]
-    WHERE
-        [area_fips] = '06073'
-        AND [year] = @year
-        -- Restrict to Private, Federal, State, Local ownership
-        AND [ownership_title] IN (
-            'Private',
-            'Federal Government',
-            'State Government',
-            'Local Government'
-        )
-        -- Restrict to two-digit NAICS codes
-        AND [industry_code] IN (
-            '11',
-            '21',
-            '22',
-            '23',
-            '31-33',
-            '42',
-            '44-45',
-            '48-49',
-            '51',
-            '52',
-            '53',
-            '54',
-            '55',
-            '56',
-            '61',
-            '62',
-            '71',
-            '721',
-            '722',
-            '81',
-            '92'
-        )
-)
-INSERT INTO [#qcew_result_set]
 SELECT
-    [ownership_title],
-    [industry_code],
-    -- Averaging and rounding occurs at final reporting step
-    -- See https://github.com/SANDAG/BLS/issues/55
-    ROUND(SUM([jobs]), 0) AS [jobs]
-FROM [annual_data]
+    [fn_get_sandag_employment].[ownership_title],
+    [fn_get_sandag_employment].[industry_code],
+	ROUND(SUM([annual_avg_emplvl]), 0) AS [jobs]
+FROM [socioec_data].[bls].[qcew_by_area_annual]
+INNER JOIN [socioec_data].[bls].[industry_code]
+	ON [qcew_by_area_annual].[naics_id] = [industry_code].[naics_id]
+INNER JOIN [socioec_data].[bls].[ownership_titles]
+	ON [qcew_by_area_annual].[own_code] = [ownership_titles].[ownership_code]
+CROSS APPLY [socioec_data].[bls].[fn_get_sandag_employment]([ownership_title], [industry_code])
+WHERE
+    [area_fips] = '06073'
+    AND [year] = @year
+    AND [fn_get_sandag_employment].[ownership_title] IS NOT NULL
+    AND [fn_get_sandag_employment].[industry_code] IS NOT NULL
+    -- "Private" ownership only categories
+    -- Are published directly in the annual QCEW
+    AND [fn_get_sandag_employment].[ownership_title] = 'Private'
 GROUP BY
-    [ownership_title],
-    [industry_code]
-HAVING [ownership_title] = 'Private'
-ORDER BY
-    [ownership_title],
-    [industry_code]
+    [fn_get_sandag_employment].[ownership_title],
+    [fn_get_sandag_employment].[industry_code]
 
-
--- Return combined result set
-SELECT
-    @year AS [year],
-    [ownership_title],
-    [industry_code],
-    [jobs]
-FROM [#qcew_result_set]
 ORDER BY
     [ownership_title],
     [industry_code]
