dc1bfdb1aefc787d28375539a177efe6b440748a
GregorSchroeder <gsc@sandag.org>
2026-08-26 14:18:40 -0700

    #297 - update military employment to allow series 16

--- sql/employment/get_military_employment.sql
+++ sql/employment/get_military_employment.sql
@@ -1,22 +1,38 @@
 /* 
-This query grabs the Military Active Duty (Job) Data and assigns counts to MGRA15.
+This query grabs the Military Active Duty (Job) Data and assigns counts to MGRAs.
 This will assign 0s to MGRAs where there are no military jobs.
 
-Notes:
-    1) This is assuming a connection to the GIS server
-    2) currently only works using MGRA15
+Note: This is assuming a connection to the GIS server
 */
 
+SET NOCOUNT ON;
 -- Initialize parameters -----------------------------------------------------
 DECLARE @run_id INTEGER = :run_id;
 DECLARE @year INTEGER = :year;
-DECLARE @series INTEGER = :series;
+DECLARE @estimates_server nvarchar(20) = :estimates_server;
+DECLARE @estimates_database nvarchar(20) = :estimates_database;
 
--- Check for MGRA series and stop execution if not Series 15
-IF @series != 15
-BEGIN
-    THROW 50000, 'Spatial join with military locations only valid for Series 15 MGRAs',1
-END
+
+-- Get MGRA geography and insert to temporary table
+-- Build the OPENQUERY to the Estimates database to get the MGRA geography
+-- Note the statement stores results in a temporary table for later use
+DROP TABLE IF EXISTS [#mgra];
+CREATE TABLE [#mgra] (
+    [mgra] INTEGER NOT NULL,
+    [shape] GEOMETRY NOT NULL,
+    CONSTRAINT [pk_tt_mgra] PRIMARY KEY ([mgra])
+);
+
+DECLARE @qry NVARCHAR(max) = '
+    INSERT INTO [#mgra]
+    SELECT [mgra], [shape]
+    FROM OPENQUERY([' + @estimates_server + '], ''
+        SELECT [mgra], [shape]
+        FROM ' + @estimates_database + '.[inputs].[mgra]
+        WHERE [run_id] = ' + CONVERT(NVARCHAR, @run_id) + '
+    '')
+'
+EXEC sp_executesql @qry;
 
 
 -- Send error message if no data exists --------------------------------------
@@ -39,13 +55,15 @@ BEGIN
         'Federal Government' AS [ownership_title],
         'MIL' AS [industry_code],
         COALESCE(SUM([site_active_duty]), 0) AS [value]
-    FROM [GeoDepot].[sde].[MGRA15]
+    FROM [#mgra]
     LEFT OUTER JOIN (
         SELECT [site_active_duty], [shape] 
         FROM [EMPCORE].[dbo].[mil_active_duty]
         WHERE [yr] = @year
         ) AS [mil_active_duty]
-        ON [mil_active_duty].[shape].STWithin([MGRA15].[shape]) = 1
+        ON [mil_active_duty].[shape].STWithin([#mgra].[shape]) = 1
     GROUP BY [mgra]
-    ORDER BY [mgra]
+    ORDER BY [mgra];
+
+    DROP TABLE IF EXISTS [#mgra];
 END
